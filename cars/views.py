5025435bc8ab0c162ecdb3a765549aa200a9168c
Roberto Moreira <robertodev04@gmail.com>
2025-10-02 15:16:57 -0300

    views da aplicação

---
+++ cars/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from cars.models import Car
+from cars.forms import CarForm
+# Create your views here.
+
+def cars_view(request):
+    cars = Car.objects.all().order_by('brand__name')
+    search = request.GET.get('search')
+ 
+    if search:
+        cars = cars.filter(model__icontains=search) #icontains ignora uppercase ou lower, sem o i ele leva em consideração
+
+    return render(request, 'cars.html', {'cars': cars }
+    )
+
+def new_car_view(request):
+    if request.method == 'POST':
+        new_car_form = CarForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+    else: 
+        new_car_form = CarForm()
+    return render(request, 'new_car.html', {'new_car_form': new_car_form})
+
