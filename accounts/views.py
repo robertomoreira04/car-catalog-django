b3884368c5ecb7644cfe4e5fda35a9ba79e79b02
Roberto Moreira <robertodev04@gmail.com>
2025-10-07 08:24:23 -0300

    criando a view de autenticação

---
+++ accounts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+
+def register_view(request):
+    if request.method == "POST":
+        user_form = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect('cars_list')
+    else:
+        user_form = UserCreationForm()
+    return render (request, 'register.html', {'user_form': user_form})
