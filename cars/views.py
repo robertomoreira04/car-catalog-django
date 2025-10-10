935ac3cd7d60fd9c6ae919c26286dbe5c74ac93e
Roberto Moreira <robertodev04@gmail.com>
2025-10-10 14:41:53 -0300

    atualizando a view cars_list de function view para class view

--- cars/views.py
+++ cars/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
 # Create your views here.
 
-def cars_view(request):
-    cars = Car.objects.all().order_by('brand__name')
-    search = request.GET.get('search')
- 
-    if search:
-        cars = cars.filter(model__icontains=search) #icontains ignora uppercase ou lower, sem o i ele leva em consideração
 
-    return render(request, 'cars.html', {'cars': cars }
-    )
+class CarsView(View):
+
+    def get(self, request):
+        cars = Car.objects.all().order_by('brand__name')
+        search = request.GET.get('search')
+ 
+        if search:
+            cars = cars.filter(model__icontains=search) #icontains ignora uppercase ou lower, sem o i ele leva em consideração
+
+        return render(
+            request, 
+            'cars.html', 
+            {'cars': cars }
+        )
+
 
 def new_car_view(request):
     if request.method == 'POST':
