108b2a6c75708e542f378c35af3a193129ad68a7
Roberto Moreira <robertodev04@gmail.com>
2025-10-13 09:40:13 -0300

    modificando o modelo de view de cadastro de carro para createview

--- cars/views.py
+++ cars/views.py
@@ -1,10 +1,8 @@
-from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 # Create your views here.
-
+ 
 class CarsListView(ListView):
     model = Car
     template_name = 'cars.html'
@@ -18,15 +16,9 @@ class CarsListView(ListView):
         return cars
 
 
-class NewCarView(View):
+class NewCarCreateView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name =  'new_car.html'
+    success_url = '/cars/'
 
-    def get(self, request):
-        new_car_form = CarModelForm()
-        return render(request, 'new_car.html', {'new_car_form': new_car_form})
-    
-    def post(self, request):
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-        return render(request, 'new_car.html', {'new_car_form': new_car_form})
