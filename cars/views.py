d40436990a6270a34d61a5ca719155d4f696125f
Roberto Moreira <robertodev04@gmail.com>
2025-10-13 09:52:06 -0300

    criando view de detalhes dos carros com detail view

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,6 @@
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView
 # Create your views here.
  
 class CarsListView(ListView):
@@ -21,4 +21,9 @@ class NewCarCreateView(CreateView):
     form_class = CarModelForm
     template_name =  'new_car.html'
     success_url = '/cars/'
+
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
 
