031952204865b6e4e2365bbbc228d3396795d86c
Roberto Moreira <robertodev04@gmail.com>
2025-10-14 10:11:55 -0300

    criando view de deletar carro

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,6 @@
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 # Create your views here.
  
 class CarsListView(ListView):
@@ -34,4 +34,8 @@ class CarUpdateView(UpdateView):
     template_name = 'car_update.html'
     success_url = '/cars/'
 
-    
+
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
+    success_url = '/cars/'
