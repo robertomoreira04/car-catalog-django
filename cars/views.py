2330775fb1a26ab0a4f3588f0432489fbba0650b
Roberto Moreira <robertodev04@gmail.com>
2025-10-14 09:50:24 -0300

    criando view de atualizar carro

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,6 @@
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views.generic import ListView, CreateView, DetailView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 # Create your views here.
  
 class CarsListView(ListView):
@@ -27,3 +27,11 @@ class CarDetailView(DetailView):
     model = Car
     template_name = 'car_detail.html'
 
+
+class CarUpdateView(UpdateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'car_update.html'
+    success_url = '/cars/'
+
+    
