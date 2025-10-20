5467c9dab3d5c6173d47b61ca8f7141dfecdbd10
Roberto Moreira <robertodev04@gmail.com>
2025-10-20 11:55:39 -0300

    criando model/tabela de inventário de carro

--- cars/models.py
+++ cars/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 class Brand(models.Model):
     id = models.AutoField(primary_key=True)
@@ -8,6 +7,7 @@ class Brand(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class Car(models.Model):
     id = models.AutoField(primary_key=True)
@@ -22,3 +22,14 @@ class Car(models.Model):
     def __str__(self):
         return self.model
     
+
+class CarInventory(models.Model):
+    cars_count = models.IntegerField()
+    cars_value = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.cars_count} - {self.cars_value}'
